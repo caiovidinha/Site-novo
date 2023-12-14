5199c416f33bf5672c871cd3578cd0aa7c90cf84
Caio Vidinha <caiovidinha@id.uff.br>
2023-12-14 10:37:45 -0300

    quase finalizando dropdown

--- scripts.js
+++ scripts.js
@@ -1,9 +1,22 @@
 const navlinks = document.querySelector('.nav-links')
-// function onToggleMenu(e){
-//     e.name = e.name === 'menu' ? 'close' : 'menu'
-//     navlinks.classList.toggle('-top-[62%]')
-//     navlinks.classList.toggle('top-[13%]')
-// }
+const selectMenu = document.querySelector('.select-menu'),
+        selectBtn = selectMenu.querySelector('.select-btn'),
+        options = selectMenu.querySelectorAll('.option'),
+        btnText = selectMenu.querySelector('.btn-text')
+
+selectBtn.addEventListener("click", () => {
+    optionMenu.classList.toggle("active")
+})
+
+options.forEach(option => {
+    option.addEventListener("click", () =>{
+        let selectedOption = option.querySelector('.option-txt').innerText
+        btnText.innerText = selectedOption
+    })
+    
+})
+
+
 function onToggleMenu(e){
     e.name = e.name === 'menu' ? 'close' : 'menu'
     navlinks.classList.toggle('left-0')
