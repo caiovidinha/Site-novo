0b4bcafacbbd0dc5d57f3c2935edcb58d0470a17
Caio Vidinha <caiovidinha@id.uff.br>
2024-01-09 14:28:01 -0300

    Falta ""backend"" e clube de vantagens

--- js/cep.js
+++ js/cep.js
@@ -1,11 +1,40 @@
-function onlyNumber(evt) {
- 
-    // Only ASCII character in that range allowed
-    let ASCIICode = (evt.which) ? evt.which : evt.keyCode
-    if (ASCIICode > 31 && (ASCIICode < 48 || ASCIICode > 57))
-        return false;
-    return true;
+function mascara(o,f){
+  v_obj=o
+  v_fun=f
+  setTimeout("execmascara()",1)
 }
+function execmascara(){
+  v_obj.value=v_fun(v_obj.value)
+}
+function mtel(v){
+  v=v.replace(/\D/g,""); //Remove tudo o que não é dígito
+  return v;
+}
+function id( el ){
+return document.getElementById( el );
+}
+window.onload = function(){
+id('cep').onkeyup = function(){
+  mascara( this, mtel );
+}
+}
+
+
+const submit = document.querySelector('button')
+
+submit.addEventListener('click', (e)=>{
+  const cep = document.querySelector('#cep').value
+  const numero = document.querySelector('#numero').value
+  e.preventDefault()
+  console.log(cep + ',' + numero)
+  if(cep.length === 8 && numero.length !== 0) console.log('sim')
+  else console.log('nao')
+})
+
+
+
+
+
 
 $(document).ready(function () {
     
@@ -13,6 +42,8 @@ $(document).ready(function () {
     $('#endereco, #numero, #bairro, #estado, #cidade').prop('disabled', true);
     // Consulta o CEP e preenche os campos de endereço
     $('#cep').blur(function () {
+      $('#cep').removeClass('border-red-700')
+      $('#errorCEP').addClass('hidden')
       $.getJSON('https://viacep.com.br/ws/' + this.value + '/json/', function (data) {
         if (!data.erro) {
           $('#endereco').val(data.logradouro);
@@ -23,7 +54,8 @@ $(document).ready(function () {
           // Habilita os campos restantes
           $('#endereco, #numero, #bairro, #estado, #cidade').prop('disabled', false);
         } else {
-          alert('CEP não encontrado.');
+          $('#cep').addClass('border-red-700')
+          $('#errorCEP').removeClass('hidden')
         }
       });
     });
