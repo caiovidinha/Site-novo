94b5958c8ce3f04d95f510f7b1e19f173871e9fa
Caio Vidinha <caiovidinha@id.uff.br>
2024-01-23 17:39:53 -0300

    https

--- js/api.js
+++ js/api.js
@@ -1,12 +1,12 @@
 const getCEP = async (cep,num) => {
-    const url = `http://www.greenfiber.net.br/api/consulta/${cep}/${num}`
+    const url = `https://www.greenfiber.net.br/api/consulta/${cep}/${num}`
     const res = await fetch(url)
     const clientList = await res.json()
     return clientList.existe
 }
 
 const getPessoa = async (login,cpf) => {
-    const url = `http://www.greenfiber.net.br/api/login/${login}/${cpf}`
+    const url = `https://www.greenfiber.net.br/api/login/${login}/${cpf}`
     const res = await fetch(url)
     const response = await res.text()
     if(JSON.parse(response).error == 'cpf') return 'Erro de CPF'
