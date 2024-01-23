501e2cb3d08c7303f03967122cfc39c35cd9d110
Caio Vidinha <caiovidinha@id.uff.br>
2024-01-23 17:14:40 -0300

    API endereço alterado

--- js/api.js
+++ js/api.js
@@ -1,12 +1,12 @@
 const getCEP = async (cep,num) => {
-    const url = `http://localhost:3333/consulta/${cep}/${num}`
+    const url = `http://www.greenfiber.net.br/api/consulta/${cep}/${num}`
     const res = await fetch(url)
     const clientList = await res.json()
     return clientList.existe
 }
 
 const getPessoa = async (login,cpf) => {
-    const url = `http://localhost:3333/login/${login}/${cpf}`
+    const url = `http://www.greenfiber.net.br/api/login/${login}/${cpf}`
     const res = await fetch(url)
     const response = await res.text()
     if(JSON.parse(response).error == 'cpf') return 'Erro de CPF'
